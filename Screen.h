1badeeb403b8ad581b5595dd284eb3d754ee1ad2
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-04-27 04:40:14 +0300

    Refactory Code structure

--- Screen.h
+++ Screen.h
@@ -3,27 +3,32 @@
 #include "ColorUtils.h"
 #include "Cursor.h"
 #include "IRunnable.h"
+#include <vector>
+
 class Screen : public IRunnable
 {
-public:
-	int Width;
-	int Height;
-	const char* name;
-	void SetConsoleScreenWidthProperty();
-	int  GetConsoleScreenWidthProperty();
-	int  GetConsoleScreenHeightProperty();
-	void SetConsoleScreenHeightProperty();
-	void SetConsoleNameProperty(const char*);
-	void SetConsoleTextColor(ColorUtils);
-	virtual void IRun() override;
-	Screen();
-	~Screen();
+	public:
+		int Width;
+		int Height;
+		const char* name;
+		int  GetConsoleScreenWidthProperty();
+		int  GetConsoleScreenHeightProperty();
+		void SetConsoleScreenWidthProperty();
+		template<typename Type>
+		void SetCursorScreenPositionProperty(Type, Type);
+		void SetConsoleScreenHeightProperty();
+		void SetConsoleNameProperty(const char*);
+		void SetConsoleTextColor(ColorUtils);
+		virtual void IRun() override;
+		Screen();
+		~Screen();
 
-private:
-	Cursor* cursor;
-protected:
-	CONSOLE_SCREEN_BUFFER_INFO buffer;
-	HANDLE output;
-	HANDLE input;
+	private:
+		Cursor* cursor;
+	protected:
+		CONSOLE_SCREEN_BUFFER_INFO buffer;
+		HANDLE output;
+		HANDLE input;
 };
+
 
